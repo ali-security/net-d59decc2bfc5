db8be1db706198029ed0120dcaccd996a75a4684
Seal <info@sealsecurity.io>
2026-09-24 06:54:48 +0000

    version

---
+++ .github/create_mod.go
@@ -0,0 +1,29 @@
+//go:build ignore
+
+// Command create_mod packages a directory into a proxy.golang.org-format
+// module zip using golang.org/x/mod/zip.CreateFromDir.
+package main
+
+import (
+	"log"
+	"os"
+
+	"golang.org/x/mod/module"
+	"golang.org/x/mod/zip"
+)
+
+func main() {
+	if len(os.Args) != 5 {
+		log.Fatal("usage: go run create_mod.go <module-path> <version> <source-dir> <output-zip>")
+	}
+	m := module.Version{Path: os.Args[1], Version: os.Args[2]}
+	f, err := os.Create(os.Args[4])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	if err := zip.CreateFromDir(f, m, os.Args[3]); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("created module zip: %s", os.Args[4])
+}
